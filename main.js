a6cb9fe87cd85e82265465faaf861829fa1270eb
Leo Souza <bhleosouza11122999@gmail.com>
2024-02-03 15:15:25 -0300

    alteração em estilo e validação de número

--- main.js
+++ main.js
@@ -4,24 +4,48 @@ const nomes = [];
 const tels = [];
 var linhas = '';
 
+function validaForm(inputStr)
+{
+    var regra = /^(\(?\d{2}\)?\s?\d{4,5}\-?\s?\d{4})$/;
+
+    return regra.test(inputStr);
+}
+
+function formataNumero(inputStr)
+{
+    const numerosTel = inputStr.replace(/\D/g, '');
+
+    if(numerosTel.length === 11)
+    {
+        return `(${numerosTel.slice(0, 2)}) ${numerosTel.slice(2, 7)}-${numerosTel.slice(7)}`;
+    } else if(numerosTel.length === 10)
+    {
+        return `(${numerosTel.slice(0, 2)}) ${numerosTel.slice(2, 6)}-${numerosTel.slice(6)}`;
+    }
+}
+
 function criaLinhas()
 {
     const inputNomeContato = document.getElementById("nome-contato");
+    const numeroFormatado = formataNumero(inputTelContato.value)
 
     if(nomes.includes(inputNomeContato.value))
     {
         alert(`Já existe um registro com o nome ${inputNomeContato.value}`);
-    }else if (tels.includes(inputTelContato.value))
+    }else if (!validaForm(inputTelContato.value))
     {
-        alert(`Já existe um registro com o telefone ${inputTelContato.value}`);
+        alert('O número de telefone inserido é inválido.');
+    }else if (tels.includes(numeroFormatado))
+    {
+        alert(`Já existe um registro com o telefone ${numeroFormatado}`);
     }else
     {
         nomes.push(inputNomeContato.value);
-        tels.push(inputTelContato.value);
+        tels.push(numeroFormatado);
 
         let linha = `<tr>`;
         linha += `<td> ${inputNomeContato.value} </td>`;
-        linha += `<td> ${inputTelContato.value} </td>`;
+        linha += `<td> ${numeroFormatado} </td>`;
         linha += `</tr>`;
 
         linhas += linha;
@@ -40,7 +64,7 @@ function atualizaTabela()
 function contaContatos()
 {
     const totalContatos = document.getElementById('total-contatos');
-    totalContatos.innerHTML = parseFloat(nomes.length);
+    totalContatos.innerHTML = parseFloat(nomes.length) > 0 ? parseFloat(nomes.length) : '---';
 }
 
 form.addEventListener("submit", function(e)
